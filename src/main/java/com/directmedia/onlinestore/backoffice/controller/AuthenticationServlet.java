f49929a0e6b83c42bcbf82eb7d6761f91a19eb74
WANG <wangqianlf@live.com>
2024-06-19 12:03:16 +0200

    création de la Servlet AuthenticationServlet dans le package controller, utiliser la méthode doPost, affichage de la page et vérification d'authentification avec if else

---
+++ src/main/java/com/directmedia/onlinestore/backoffice/controller/AuthenticationServlet.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.directmedia.onlinestore.backoffice.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author wangq
+ */
+@WebServlet(name = "AuthenticationServlet", urlPatterns = {"/login"})
+public class AuthenticationServlet extends HttpServlet {
+
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        //On va récupérer les valeurs des deux champs de saisie.
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        
+        PrintWriter out = response.getWriter();
+        out.print("<html><body>");
+        //On va donc vérifier 
+        if (login.equals("michel")&&password.equals("123456")||login.equals("caroline")&&password.equals("abcdef")){
+           
+            //Quand l'authentification est un succès, on écrit un lien pour retourner à l'accueil
+           out.print("Bonjour " + login + " ! Vous êtes connecté(e) avecc succès !<br>");
+           out.print("<a href=\"home\">Retourner à la page d'accueil</a>");
+        }
+        else{
+           out.print("l'identifiant et/ou le mot de passe erroné(s)<br>");
+           out.print("<a href=\"login.html\">Vous pouvez réessayer de vous connecter en cliquant sur ce lien !</a>");
+        }
+        out.print("</body></html>");
+    }
+
+}
